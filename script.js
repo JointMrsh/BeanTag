f61340ec173d08635018f058c74cb333fd8f69f9
JointMrsh <117666461+JointMrsh@users.noreply.github.com>
2023-03-10 10:27:52 -0500

    Update script.js
    Fixed the problem when a new visitor visits the website it will now actually send a message.

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 function sendMessage() {
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://discordapp.com/api/webhooks/1083201311106932867/HmnLsV9wzB376hayBjEOysJ-Mzyx8IVj-Nah4AQIdkjw84WBaQ8RZpDnK747E-7v_aWM");
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://discordapp.com/api/webhooks/1083201311106932867/HmnLsV9wzB376hayBjEOysJ-Mzyx8IVj-Nah4AQIdkjw84WBaQ8RZpDnK747E-7v_aWM");
 
-    request.setRequestHeader('Content-type', 'application/json');
+  request.setRequestHeader('Content-type', 'application/json');
 
-    const params = {
-      username: "New Website Visitor!",
-      avatar_url: "",
-      content: "Someone new has explored the Bean Tag Webpage!"
+  const params = {
+    username: "New Website Visitor!",
+    avatar_url: "",
+    content: "Someone new has explored the Bean Tag Webpage!"
+  };
+
+  request.onload = function() {
+    if (request.status === 200) {
+      console.log('Message sent successfully.');
+    } else {
+      console.error('Error sending message.');
     }
+  };
 
-    request.send(JSON.stringify(params));
+  request.send(JSON.stringify(params));
 }
 
-if(document.cookie = "HasVisited=true"){
-   // User has visited.
-}
-else {
-    sendMessage()
-   document.cookie = "HasVisited=true"
+if (document.cookie == "HasVisited=true") {
+  // User has visited.
+} else {
+  sendMessage();
+  document.cookie = "HasVisited=true";
 }
